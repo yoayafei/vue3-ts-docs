1b34c3fd13f774eaf8dabbd89fe4a51b65064a6a
gao <2813314009@qq.com>
2024-10-08 19:03:27 +0800

    1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,14 +1,45 @@
+import { defineConfig } from "vitepress";
+
 export default defineConfig({
-    themeConfig:{
-        sidebar:{
-            "/guide/":[
-                collapsible: true,
-                items:[
-                    { text: "介绍", link: "/guide/" },
-                    { text: "安装", link: "/guide/installation" },
-                    { text: "基本概念", link: "/guide/concepts" },
-                ],
-            ],
-        }
-    }
-})
+  themeConfig: {
+    logo: "assets/logo.png",
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "指南", link: "/guide/" },
+      { text: "组件", link: "/vue-ts-demo/" },
+      { text: "API参考", link: "/api/" },
+      { text: "常见问题", link: "/faq/" },
+    ],
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+    sidebar: {
+      "/guide/": [
+        {
+          text: "开始",
+          collapsible: true,
+          items: [
+            { text: "介绍", link: "/guide/" },
+            { text: "安装", link: "/guide/installation" },
+            { text: "基本概念", link: "/guide/concepts" },
+          ],
+        },
+      ],
+      "/vue-ts-demo/": [
+        {
+          text: "常用组件",
+          items: [
+            { text: "介绍", link: "/vue-ts-demo/" },
+            { text: "按钮 Button", link: "/vue-ts-demo/button" },
+            { text: "表单 Form", link: "/vue-ts-demo/form" },
+            { text: "表格 Table", link: "/vue-ts-demo/table" },
+          ],
+        },
+      ],
+    },
+    footer: {
+      message: "用心学习 Vue 3 和 TypeScript!",
+      copyright: "Copyright 2024 gao",
+    },
+  },
+});
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,9 @@
 import DefaultTheme from "vitepress/theme";
 
-import "vitepress-theme-demoblock/dist/styles/index.css";
+// import "vitepress-theme-demoblock/dist/styles/index.css";
 
 import "./styles/var.css";
 
 export default {
   ...DefaultTheme,
-  enhanceApp({ app }) {},
 };
